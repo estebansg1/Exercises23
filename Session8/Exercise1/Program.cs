71faaed7f8b68f5172133c7485834e789ac8b911
estebansg1 <estebandesa0@gmail.com>
2023-03-09 11:16:54 -0600

    Add exercise1 session8

---
+++ Session8/Exercise1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Program
+{
+    class Rectangle
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the width: ");
+            double width = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter the height: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            double perimeter = Math.Abs(width + width + height + height);
+            double Area = Math.Abs(width * height);
+            double Diagonal = Math.Sqrt(Math.Pow(width, 2) + Math.Pow(height, 2));
+            {
+                Console.WriteLine("The perimeter is {0}", perimeter);
+                Console.WriteLine("The area is {0}", Area);
+                Console.WriteLine("The Diagonal es {0}", Diagonal);
+            }
+        }
+    }
+}
