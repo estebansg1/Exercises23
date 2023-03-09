e7143b337c29f02ca05fc8efc7d0e53302156559
estebansg1 <estebandesa0@gmail.com>
2023-03-09 11:35:27 -0600

    add Exercise2 Session8

--- Session8/Exercise2/Program.cs
+++ Session8/Exercise2/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Person
+{
+
+    public int age;
+    public void SetAge(int n)
+    {
+        age = n;
+    }
+    public void Greet()
+    {
+        Console.WriteLine("Person: Hello!");
+    }
+}
+
+public class Student : Person
+{
+    public void GoToClasses()
+    {
+        Console.WriteLine("Student: I'm going to class.");
+    }
+
+    public void ShowAge()
+    {
+        Console.WriteLine("Student: My age is " + age + " years old");
+    }
+}
+
+public class Teacher : Person
+{
+    public void Explain()
+    {
+        Console.WriteLine("Teacher: Explanation begins.");
+    }
+}
+
+public class StudentAndTeacherTest
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person();
+        person.Greet();
+
+        Student student = new Student();
+        student.SetAge(21);
+        student.Greet();
+        student.ShowAge();
+        student.GoToClasses();
+
+        Teacher teacher = new Teacher();
+        teacher.SetAge(30);
+        teacher.Greet();
+        teacher.Explain();
+    }
+}
