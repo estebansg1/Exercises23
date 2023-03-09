6189d56425c08399e185866223180e7131f9b0b5
estebansg1 <estebandesa0@gmail.com>
2023-03-09 02:41:37 -0600

    add ex2

---
+++ Session7/Programtwo.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace OddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 99; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
