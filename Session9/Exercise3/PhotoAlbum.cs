21ba8d6c00c078ff830dc02ba7289cdd01b6c4f0
estebansg1 <estebandesa0@gmail.com>
2023-03-20 16:04:57 -0600

    Add Exercise 3 Session9

---
+++ Session9/Exercise3/PhotoAlbum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class PhotoAlbum
+{
+    private int numberOfPages;
+
+    public PhotoAlbum()
+    {
+        numberOfPages = 16;
+    }
+
+    public PhotoAlbum(int pages)
+    {
+        numberOfPages = pages;
+    }
+    
+    public int  GetNumberOfPages()
+    {
+        return numberOfPages;
+    }
+}
+
+public class BigPhotoAlbum : PhotoAlbum
+{
+    public BigPhotoAlbum() : base(64)
+    {
+    }
+}
--- Session9/Exercise3/Program.cs
+++ Session9/Exercise3/Program.cs
@@ -1,1 +1,13 @@
-﻿
+public class AlbumTest
+{
+    public static void Main(string[] args)
+    {
+        PhotoAlbum defaultAlbum = new PhotoAlbum();
+        PhotoAlbum customAlbum = new PhotoAlbum(24);
+        BigPhotoAlbum bigAlbum = new BigPhotoAlbum();
+
+        Console.WriteLine("Default album has {0} pages.", defaultAlbum.GetNumberOfPages());
+        Console.WriteLine("Custom album has {0} pages.", customAlbum.GetNumberOfPages());
+        Console.WriteLine("Big album has {0} pages.", bigAlbum.GetNumberOfPages());
+    }
+}
