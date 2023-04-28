06554e1ee409dc6710ca9361862d887281dcbf21
estebansg1 <estebandesa0@gmail.com>
2023-04-28 09:22:58 -0600

    add Examples Session21

---
+++ Session21/Example2/Program.cs
@@ -0,0 +1,16 @@
+﻿string GetLongestWord(string filePath)
+{
+    string content = File.ReadAllText(filePath);
+    string[] words = content.Split(' ');
+    string longestWord = "";
+
+    foreach (string word in words)
+    {
+        if (word.Length > longestWord.Length)
+        {
+            longestWord = word;
+        }
+    }
+
+    return longestWord; 
+}
