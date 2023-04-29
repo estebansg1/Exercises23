50a686abaeffcdca4114e62000e3c0653a909719
estebansg1 <estebandesa0@gmail.com>
2023-04-29 00:18:02 -0600

    add Exercise1 Session18

---
+++ Session18/Exercise1/Program.cs
@@ -0,0 +1,53 @@
+﻿
+string[] words = { "apple", "banana", "cherry", "grape", "lemon" };
+Random random = new Random();
+string word = words[random.Next(0, words.Length)];
+char[] display = new char[word.Length];
+
+for (int i = 0; i < display.Length; i++)
+{
+    display[i] = '*';
+}
+
+int remainingAttempts = 3;
+bool wordGuessed = false;
+
+while (remainingAttempts > 0 && !wordGuessed)
+{
+    Console.WriteLine("Guess a letter:");
+    char guess = Console.ReadLine()[0];
+
+    bool letterFound = false;
+
+    for (int i = 0; i < word.Length; i++)
+    {
+        if (word[i] == guess)
+        {
+            display[i] = guess;
+            letterFound = true;
+        }
+    }
+
+    if (!letterFound)
+    {
+        remainingAttempts--;
+    }
+
+    Console.WriteLine(display);
+
+    if (new string(display) == word)
+    {
+        wordGuessed = true;
+    }
+}
+
+if (wordGuessed)
+{
+    Console.WriteLine("You won!");
+}
+else
+{
+    Console.WriteLine("Game over.");
+}
+
+Console.ReadLine();
